f1c70c98a9034a7a7d05f520d7914b2bde5e657d
Sylvain <sylvduthor@gmail.com>
2019-08-01 14:54:12 +0200

    Sylvain v6.3

--- src/app/file-attente/file-attente.page.ts
+++ src/app/file-attente/file-attente.page.ts
@@ -15,22 +15,40 @@ export class FileAttentePage implements OnInit {
   }
   appuyé = true;
   nbattente = 0;
+  nbminute = 0;
+  nbseconde = 0;
   
   button(){
     
     if (this.appuyé == true){
     $("#background").removeClass("unclicked");
     $("#background").addClass("clicked");
+
+    $("#joinText").css("visibility","visible");
+    $("#valeurNombre").css("visibility","visible");
+    $("#valeurTemps").css("visibility","visible");
+
+    $("#joinText").text("Vous avez rejoint la file d'attente")
+    $("#joinText").css("color","red");
     this.appuyé = false;
-    this.nbattente +=1; 
+    this.nbattente +=1;
+    this.nbminute +=5; 
+    this.nbseconde +=15;   
   }
   else {
     $("#background").removeClass("clicked");
     $("#background").addClass("unclicked");
+
+  $("#joinText").text("Vous avez quitté la file d'attente");
+  $("#joinText").css("color","blue");
     this.appuyé = true;
     this.nbattente -=1; 
+    this.nbminute -=5;  
+    this.nbseconde -=15; 
   }
-  $("#valeurNombre").text(this.nbattente)
+
+  $("#valeurNombre").text(this.nbattente);
+  $("#valeurTemps").text(this.nbminute+"min "+ this.nbseconde+"sec");
 
 }
 }
