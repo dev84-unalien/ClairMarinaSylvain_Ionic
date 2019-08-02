bc605e5745e6084392fb9f3fc928c82136be9c0b
Claire16 <claire.vogel16@gmail.com>
2019-08-02 12:22:42 +0200

    claire_v4.4

--- src/app/services/SecurityService.ts
+++ src/app/services/SecurityService.ts
@@ -7,9 +7,27 @@ export class SecurityService {
   nombreDeCarPassword = 8;
 
   constructor() {}
+  verifyNameMessage(name: string) {
+    if (!name) {
+      $("#inc_name").css("display", "flex");
+    } else {
+      $("#inc_name").css("display", "none");
+    }
+  }
+
+  verifySurnameMessage(surname: string) {
+    if (!surname) {
+      $("#inc_surname").css("display", "flex");
+    } else {
+      $("#inc_surname").css("display", "none");
+    }
+  }
+
   verifyEmailMessage(email: string) {
     if (!email) {
       $("#inc_email").css("display", "flex");
+    } else {
+      $("#inc_email").css("display", "none");
     }
   }
 
@@ -22,20 +40,45 @@ export class SecurityService {
 
     if (!password) {
       $("#inc_password").css("display", "flex");
+    } else {
+      $("#inc_password").css("display", "none");
     }
 
     if (confirmPassword.length < this.nombreDeCarPassword) {
       $("#inc_confirmpassword").css("display", "flex");
+    } else {
+      $("#inc_confirmpassword").css("display", "none");
     }
   }
 
-  checkPassword(password: string) {
+  checkName(name: string) {
     let check = false;
-    if (password && password.length > this.nombreDeCarPassword) {
+    if (name) {
       check = true;
     }
     return check;
   }
+
+  checkSurname(surname: string) {
+    let check = false;
+    if (surname) {
+      check = true;
+    }
+    return check;
+  }
+
+  checkPassword(password: string) {
+    let check = false;
+    if (password && password.length >= this.nombreDeCarPassword) {
+      check = true;
+    }
+    return check;
+  }
+
+  checkPasswordAndCofirm(password: string, confirmPassword: string) {
+    return password == confirmPassword;
+  }
+
   checkEmail(email: string) {
     let check = false;
     if (email) {
