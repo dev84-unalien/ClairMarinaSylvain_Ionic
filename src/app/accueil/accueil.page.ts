76c9b9c398cff3a43c565350ba253353e7f7d401
Claire16 <claire.vogel16@gmail.com>
2019-08-05 16:12:58 +0200

    claire_v5.3

--- src/app/accueil/accueil.page.ts
+++ src/app/accueil/accueil.page.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-<<<<<<< HEAD
-import { QRScanner, QRScannerStatus } from "@ionic-native/qr-scanner/ngx";
-import * as $ from "jquery";
 import { UserService } from "../services/user.service";
-=======
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
-import { Platform } from '@ionic/angular';
-import { GoogleMaps, GoogleMap, Environment } from '@ionic-native/google-maps/ngx';
-import * as $ from 'jquery';
->>>>>>> 0d46857e99d8597431f4cc66b031a413f69e38d1
+import { QRScanner, QRScannerStatus } from "@ionic-native/qr-scanner/ngx";
+import { Platform } from "@ionic/angular";
+import {
+  GoogleMaps,
+  GoogleMap,
+  Environment
+} from "@ionic-native/google-maps/ngx";
+import * as $ from "jquery";
 
 @Component({
   selector: "app-accueil",
@@ -16,72 +15,59 @@ import * as $ from 'jquery';
   styleUrls: ["./accueil.page.scss"]
 })
 export class AccueilPage implements OnInit {
-
   connection: any;
   connexion: any;
 
   email: string;
-  map: GoogleMap;  
+  map: GoogleMap;
+  UserService: any;
 
-<<<<<<< HEAD
-  constructor(public userServ: UserService) {}
-=======
-  constructor(private platform: Platform) { }
->>>>>>> 0d46857e99d8597431f4cc66b031a413f69e38d1
+  constructor(private platform: Platform) {}
 
   async ngOnInit() {
     await this.platform.ready();
   }
 
-<<<<<<< HEAD
-  ionViewWillLeave() {}
+ 
 
-  ionViewDidEnter() {
-    this.icone();
-  }
-
+ 
   icone() {
-    if (this.userServ.isConnected()) {
+    if (this.UserService.isConnected()) {
       this.cacheIconeUtilisateur();
     } else {
       this.afficheIconeUtilisateur();
-=======
-  ionViewWillLeave() { }
-
+    }
+  }
+  ionViewWillLeave() {}
 
   ionViewDidEnter() {
     let user = JSON.parse(localStorage.user);
-    this.email = user.email;    
-    
+    this.email = user.email;
+    this.icone();
     $("#map_canvas").hide();
-      this.showMap = false;
+    this.showMap = false;
   }
 
-  showMap= false;
+  showMap = false;
 
   loadMap() {
-    if (Environment){
+    if (Environment) {
       Environment.setEnv({
-        'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBb-oaPjtNbVzZ_1YEoy_3k09z6KVdgOCQ',
-        'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBb-oaPjtNbVzZ_1YEoy_3k09z6KVdgOCQ',
+        API_KEY_FOR_BROWSER_RELEASE: "AIzaSyBb-oaPjtNbVzZ_1YEoy_3k09z6KVdgOCQ",
+        API_KEY_FOR_BROWSER_DEBUG: "AIzaSyBb-oaPjtNbVzZ_1YEoy_3k09z6KVdgOCQ"
       });
-      
     }
-    
-    
+
     if (this.showMap == false) {
       $("#fond").hide();
       $("#map_canvas").show();
-      this.map = GoogleMaps.create('map_canvas');
-    //  Environment.setBackgroundColor('red');
+      this.map = GoogleMaps.create("map_canvas");
+      //  Environment.setBackgroundColor('red');
       this.showMap = true;
-    }
-
-    else {
+    } else {
       $("#fond").show();
       $("#map_canvas").hide();
       this.showMap = false;
->>>>>>> 0d46857e99d8597431f4cc66b031a413f69e38d1
     }
   }
 
@@ -89,19 +75,11 @@ export class AccueilPage implements OnInit {
     const context = this;
     let qr = new QRScanner();
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0d46857e99d8597431f4cc66b031a413f69e38d1
     qr.prepare()
       .then((status: QRScannerStatus) => {
         if (status.authorized) {
-          // camera permission was granted
+          // camera permission was gran
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0d46857e99d8597431f4cc66b031a413f69e38d1
           // start scanning
           let scanSub = qr.scan().subscribe((text: string) => {
             context.resultQR(text);
@@ -112,10 +90,6 @@ export class AccueilPage implements OnInit {
 
           qr.show();
           $("body").hide();
-<<<<<<< HEAD
-=======
-
->>>>>>> 0d46857e99d8597431f4cc66b031a413f69e38d1
         } else if (status.denied) {
           // camera permission was permanently denied
           // you must use QRScanner.openSettings() method to guide the user to the settings page
@@ -124,15 +98,12 @@ export class AccueilPage implements OnInit {
           // permission was denied, but not permanently. You can ask for permission again at a later time.
         }
       })
-<<<<<<< HEAD
+
       .catch((e: any) => console.log("Error is", e));
   }
 
   resultQR(resultat) {
     alert(resultat);
-=======
-      .catch((e: any) => console.log('Error is', e));
->>>>>>> 0d46857e99d8597431f4cc66b031a413f69e38d1
   }
 
   cacheIconeUtilisateur() {
@@ -140,15 +111,10 @@ export class AccueilPage implements OnInit {
     $("#connect").css("display", "none");
   }
 
-<<<<<<< HEAD
   afficheIconeUtilisateur() {
     // Sinon display flex icone
     $("#connect").css("display", "flex");
-=======
-  resultQR(resultat) {
-    alert(resultat);
-
->>>>>>> 0d46857e99d8597431f4cc66b031a413f69e38d1
   }
+
 
 }
